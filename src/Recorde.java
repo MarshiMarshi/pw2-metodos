347cbb5d7c6a1d23996c35a348f084d7ba25d3fe
Gustavo GR <gustavogr1806@gmail.com>
2022-05-16 11:03:15 -0300

    Tchau

---
+++ src/Recorde.java
@@ -0,0 +1,17 @@
+
+public class Recorde {
+
+	public Recorde (TipoRecordeEnum tipo) {
+		this.tipo = tipo;
+	}
+	
+	public int valor;
+	public TipoRecordeEnum tipo;
+	
+	public void obterResultado(int valor) {
+		
+		/*if () {
+			
+		}*/
+	}
+}
---
+++ src/TestaRecorde.java
@@ -0,0 +1,12 @@
+
+public class TestaRecorde {
+
+	public static void main(String[] args) {
+		
+		int recordeObtido;
+		
+		Recorde recordeMundial = new Recorde(TipoRecordeEnum.MUNDIAL);
+		
+		Recorde recordeOlimpico = new Recorde(TipoRecordeEnum.OLIMPICO);
+	}
+}
